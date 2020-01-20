561d093799bb266f46b7f209100541ed6ffaab05
daddyawesome <sablay296@gmail.com>
2020-01-20 19:23:35 +0000

    finishing app.py

--- app.py
+++ app.py
@@ -47,45 +47,23 @@ app.layout = html.Div([
 
 ])
 
+#############################################
+# Interaction Between Components / Controller
+#############################################
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# Template
+@app.callback(
+    Output(component_id='selector-id', component_property='figure'),
+    [
+        Input(component_id='input-selector-id', component_property='value')
+    ]
+)
+def ctrl_func(input_selection):
+    return None
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
-])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8080, host='0.0.0.0')
+    app.run_server(
+        debug=True, 
+        port=8080, 
+        host='0.0.0.0')
